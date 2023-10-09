9e2e7d5657b4c6bd12d6ed76dd91d184cd9fcee5
Matt Chapman <matt.chapman@harmoney.co.nz>
2023-10-09 20:17:21 +1300

    Remove excess code in main

--- lib/main.dart
+++ lib/main.dart
@@ -16,55 +16,15 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Chartr',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
         useMaterial3: true,
       ),
       home: const FullScreenMapWidget(),
-      // home: PaintCanvas(),
-    );
-  }
-}
-
-class PaintCanvas extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() => PaintCanvasState();
-}
-
-class PaintCanvasState extends State<PaintCanvas> {
-  List<Offset> points = [];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(children: [
-       Column(children: [Text("Hello")],),
-        GestureDetector(
-          behavior: HitTestBehavior.opaque,
-          onPanUpdate: (details) {
-            setState(() {
-              final renderBox = context.findRenderObject() as RenderBox;
-              final localPosition =
-              renderBox.globalToLocal(details.globalPosition);
-              print(localPosition);
-              points.add(localPosition);
-            });
-            print("DRAW");
-          },
-          onPanEnd: (_) {
-            // Drawing finished
-          },
-          child: CustomPaint(
-            size: Size.infinite,
-            painter: CanvasPainter(points, Colors.black),
-          ),
-        ),
-      ],),
     );
   }
 }
