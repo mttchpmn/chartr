25889f526e49da68d2bcfb187d83f28c7564ac36
Matt Chapman <matt.chapman@harmoney.co.nz>
2023-10-10 21:11:08 +1300

    Fix draw buttons

--- lib/components/paint_layer/draw_button_stack.dart
+++ lib/components/paint_layer/draw_button_stack.dart
@@ -7,16 +7,15 @@ class DrawButtonStack extends StatelessWidget {
   final VoidCallback onColorChange;
   Color paintColor;
 
-  final Color _iconColor = const Color(0xFF41548C);
+  final Color _iconColor = Colors.deepOrange;
 
-  DrawButtonStack({
-    super.key,
-    required this.onDrawCancel,
-    required this.onDrawClear,
-    required this.onDrawSave,
-    required this.onColorChange,
-    required this.paintColor
-  });
+  DrawButtonStack(
+      {super.key,
+      required this.onDrawCancel,
+      required this.onDrawClear,
+      required this.onDrawSave,
+      required this.onColorChange,
+      required this.paintColor});
 
   @override
   Widget build(BuildContext context) {
@@ -25,36 +24,39 @@ class DrawButtonStack extends StatelessWidget {
         top: 50, // Adjust the position as needed
         right: 15,
         child: FloatingActionButton(
+          backgroundColor: Colors.black,
           onPressed: onDrawCancel,
           mini: true,
-          child: Icon(Icons.cancel, color: _iconColor),
+          child: Icon(Icons.clear, color: _iconColor),
         ),
       ),
+      // Positioned(
+      //   top: 100, // Adjust the position as needed
+      //   right: 15,
+      //   child: FloatingActionButton(
+      //     onPressed: onDrawClear,
+      //     mini: true,
+      //     child: Icon(Icons.clear, color: _iconColor),
+      //   ),
+      // ),
       Positioned(
         top: 100, // Adjust the position as needed
         right: 15,
         child: FloatingActionButton(
-          onPressed: onDrawClear,
+          backgroundColor: Colors.black,
+          onPressed: onDrawSave,
           mini: true,
-          child: Icon(Icons.clear, color: _iconColor),
+          child: Icon(Icons.check, color: _iconColor),
         ),
       ),
       Positioned(
         top: 150, // Adjust the position as needed
         right: 15,
         child: FloatingActionButton(
-          onPressed: onDrawSave,
-          mini: true,
-          child: Icon(Icons.save, color: _iconColor),
-        ),
-      ),
-      Positioned(
-        top: 200, // Adjust the position as needed
-        right: 15,
-        child: FloatingActionButton(
+          backgroundColor: paintColor,
           onPressed: onColorChange,
           mini: true,
-          child: Icon(Icons.format_paint, color: paintColor),
+          child: Icon(Icons.brush, color: Colors.black),
         ),
       ),
     ]);
