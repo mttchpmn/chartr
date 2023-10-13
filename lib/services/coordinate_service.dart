cac4fae1ab570d68f02a7d76250a44c34e61a947
Matt Chapman <matt.chapman@harmoney.co.nz>
2023-10-13 18:38:31 +1300

    Fix coord service

--- lib/services/coordinate_service.dart
+++ lib/services/coordinate_service.dart
@@ -7,7 +7,8 @@ class CoordinateService {
 
     var nztmDefinition =
         "+proj=tmerc +lat_0=0 +lon_0=173 +k=0.9996 +x_0=1600000 +y_0=10000000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs +type=crs";
-    var nztmProjection = Projection.add("EPSG:2193", nztmDefinition);
+    var nztmProjection = Projection.get("EPSG:2193") ??
+        Projection.add("EPSG:2193", nztmDefinition);
 
     var point = Point(
       x: latLng.longitude,
