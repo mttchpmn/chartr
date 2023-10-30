406cdf2a8db4012e680727792afee37d7369ef9c
Matt Chapman <matt.chapman@harmoney.co.nz>
2023-10-30 21:54:04 +1300

    Add method to convert grid to lat lng

--- lib/services/coordinate_service.dart
+++ lib/services/coordinate_service.dart
@@ -3,13 +3,14 @@ import 'package:latlong2/latlong.dart';
 import 'package:proj4dart/proj4dart.dart';
 
 class CoordinateService {
+  final String _nztmProjectionDefinition =
+      "+proj=tmerc +lat_0=0 +lon_0=173 +k=0.9996 +x_0=1600000 +y_0=10000000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs +type=crs";
+
   GridRef latLngToGrid(LatLng latLng) {
     var baseProjection = Projection.get('EPSG:4326')!;
 
-    var nztmDefinition =
-        "+proj=tmerc +lat_0=0 +lon_0=173 +k=0.9996 +x_0=1600000 +y_0=10000000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs +type=crs";
     var nztmProjection = Projection.get("EPSG:2193") ??
-        Projection.add("EPSG:2193", nztmDefinition);
+        Projection.add("EPSG:2193", _nztmProjectionDefinition);
 
     var point = Point(
       x: latLng.longitude,
@@ -20,5 +21,18 @@ class CoordinateService {
 
     return GridRef(result.x.round(), result.y.round());
   }
-}
 
+  LatLng gridRefToLatLng(GridRef gridRef) {
+    var point =
+        Point(x: gridRef.eastings.toDouble(), y: gridRef.northings.toDouble());
+
+    var baseProjection = Projection.get("EPSG:2193") ??
+        Projection.add("EPSG:2193", _nztmProjectionDefinition);
+
+    var latLngProjection = Projection.get('EPSG:4326')!;
+
+    var result = baseProjection.transform(latLngProjection, point);
+
+    return LatLng(result.y, result.x);
+  }
+}
