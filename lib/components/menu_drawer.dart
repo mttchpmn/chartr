3a2f9659a774bfd0c4f98ebcd3071aa693dea6ac
Matt Chapman <matt@mattchapman.io>
2023-11-08 12:57:38 +1300

    Update menu

--- lib/components/menu_drawer.dart
+++ lib/components/menu_drawer.dart
@@ -8,6 +8,43 @@ class MenuDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
+      child: ListView(
+        // Important: Remove any padding from the ListView.
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          const DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.deepOrange,
+            ),
+            child: Text(
+              'RANGR',
+              style: TextStyle(fontSize: 32),
+            ),
+          ),
+          ListTile(
+            leading: const Icon(Icons.map),
+            title: const Text(
+              'NAVIGATION',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            onTap: () {
+              Navigator.of(context).pushNamed("/home");
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.sunny),
+            title: const Text(
+              'WEATHER',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            onTap: () {
+              Navigator.of(context).pushNamed("/weather");
+            },
+          ),
+        ],
+      ),
+    );
+    return Drawer(
       child: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Column(
