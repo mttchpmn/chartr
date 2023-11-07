7d91d0917dfec3da0cbe580c13261fdd0369339f
Matt Chapman <matt@mattchapman.io>
2023-11-08 12:51:30 +1300

    Support changing weather days

--- lib/views/weather_screen.dart
+++ lib/views/weather_screen.dart
@@ -5,6 +5,7 @@ import 'package:chartr/services/location_service.dart';
 import 'package:chartr/services/mapbox_service.dart';
 import 'package:chartr/services/weather_service.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:latlong2/latlong.dart';
 
 class WeatherScreen extends StatefulWidget {
@@ -17,15 +18,47 @@ class _WeatherScreenState extends State<WeatherScreen> {
   final LocationService _locationService = LocationService(0);
   final MapboxService _mapboxService = MapboxService();
 
+  late DateTime _date;
   WeatherPointData? _weatherPointData;
+  bool _weatherPointDataLoading = false;
   LatLng? _deviceLocation;
   String? _locality;
 
   @override
   void initState() {
     super.initState();
+    _setDate();
     _setLocation();
-    _initWeatherPointData();
+    _setWeatherPointData();
+  }
+
+  void _setDate() {
+    var now = DateTime.now(); // Get current UTC time.
+    var midnightThisMorning = DateTime(now.year, now.month, now.day);
+
+    setState(() {
+      _date = midnightThisMorning;
+    });
+  }
+
+  void _setDateNextDay() {
+    var newDate = _date.add(Duration(days: 1));
+
+    setState(() {
+      _date = newDate;
+    });
+
+    _setWeatherPointData();
+  }
+
+  void _setDatePrevDay() {
+    var newDate = _date.subtract(Duration(days: 1));
+
+    setState(() {
+      _date = newDate;
+    });
+
+    _setWeatherPointData();
   }
 
   void _setLocation() async {
@@ -40,36 +73,31 @@ class _WeatherScreenState extends State<WeatherScreen> {
     });
   }
 
-  void _initWeatherPointData() async {
-    var now = DateTime.now(); // Get current UTC time.
-    var midnightLastNight = DateTime(now.year, now.month, now.day);
-    var isoDateStr =
-        midnightLastNight.toUtc().toIso8601String(); // Convert to ISO string.
-
-    print("Weather from: $isoDateStr");
-
+  void _setWeatherPointData() async {
+    setState(() {
+      _weatherPointDataLoading = true;
+    });
     var input = WeatherPointDataInput(
         point: const LatLng(-36.861851, 174.852992),
-        fromDate: isoDateStr,
+        fromDate: _date.toUtc().toIso8601String(),
         interval: TimeInterval.oneHourly,
         repeat: 24);
     var data = await _weatherService.getWeatherPointData(input);
 
     setState(() {
       _weatherPointData = data;
+      _weatherPointDataLoading = false;
     });
+  }
+
+  String _getDate() {
+    return DateFormat('EEE dd MMMM').format(_date);
   }
 
   @override
   Widget build(BuildContext context) {
     if (_weatherPointData == null || _deviceLocation == null) {
-      return const Scaffold(
-          body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(child: Text("Loading...")),
-        ],
-      ));
+      return const Scaffold(body: Spinner());
     }
 
     return Scaffold(
@@ -86,11 +114,41 @@ class _WeatherScreenState extends State<WeatherScreen> {
               "Showing Weather for ${_locality ?? "Your Location"}",
               style: TextStyle(fontSize: 20),
             ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                IconButton(
+                    onPressed: _setDatePrevDay,
+                    icon: Icon(Icons.arrow_circle_left)),
+                Text(_getDate()),
+                IconButton(
+                    onPressed: _setDateNextDay,
+                    icon: Icon(Icons.arrow_circle_right))
+              ],
+            ),
             SizedBox(
               height: 15,
             ),
-            WeatherPointChart(weatherData: _weatherPointData!)
+            _weatherPointDataLoading
+                ? const Spinner()
+                : WeatherPointChart(weatherData: _weatherPointData!)
           ]),
         ));
+  }
+}
+
+class Spinner extends StatelessWidget {
+  const Spinner({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Center(child: Text("Loading...")),
+      ],
+    );
   }
 }
